01183ddc6ca2bff691300309cbb343a8a6f68500
Keven Jesus <falecom@keven.com.br>
2025-05-02 08:11:40 -0300

    feat: leads

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -13,6 +13,7 @@ export default function Header() {
             <img  style={{width: '200px', cursor: 'pointer'}} onClick={() => router.push('/admin')} src="https://c6energy.com.br/wp-content/uploads/2024/08/logo-c6-energy-colorido2-copiar.webp" alt="" />
             
                     <S.Menu>
+                        {user && <S.MenuItem onClick={() => router.push('/admin')}>Leads</S.MenuItem>}
                         {user?.role_admin.name === 'admin' &&<S.MenuItem onClick={() => router.push('/admin/partner')}>Vendedores</S.MenuItem>}
                         {user && <S.MenuItem onClick={() => signOut()}>Sair</S.MenuItem>}
                     </S.Menu>
